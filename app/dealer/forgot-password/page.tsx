67145788e75f448a56fa766ea1d66be2b8ae94be
Roman <freak-009@mail.com>
2026-03-01 20:39:05 +0500

    changes

---
+++ app/dealer/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="rounded-[18px] border border-black/10 bg-white p-6 text-sm text-black/60">
+      Страница “Восстановить пароль” в разработке.
+    </div>
+  );
+}
---
+++ app/dealer/reset-password/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="rounded-[18px] border border-black/10 bg-white p-6 text-sm text-black/60">
+      Страница “Сброс пароля” в разработке.
+    </div>
+  );
+}
